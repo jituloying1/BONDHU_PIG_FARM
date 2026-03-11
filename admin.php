70d4f533f63290bb2e3d49cebdc2e166ecc227b9
jituloying1 <jituloying1947@gmail.com>
2026-03-11 14:06:35 +0530

    Update admin.php

--- admin.php
+++ admin.php
@@ -2,55 +2,23 @@
 
 $conn = new mysqli("localhost","root","","userform");
 
+header("Content-Type: application/xls");
+header("Content-Disposition: attachment; filename=user_data.xls");
+
 $result = $conn->query("SELECT * FROM users");
 
-?>
-
-<h2>Admin Panel</h2>
-
-<a href="export.php">
-<button>Download Excel Sheet</button>
-</a>
-
-<table border="1">
-
-<tr>
-<th>Name</th>
-<th>Father Name</th>
-<th>Mobile</th>
-<th>Email</th>
-<th>Qualification</th>
-<th>Photo</th>
-<th>Signature</th>
-<th>Marksheet</th>
-<th>Caste Certificate</th>
-</tr>
-
-<?php
+echo "Name\tFather Name\tMobile\tEmail\tQualification\tDistrict\tState\tPincode\n";
 
 while($row = $result->fetch_assoc())
 {
-
-echo "<tr>
-
-<td>".$row['name']."</td>
-<td>".$row['father_name']."</td>
-<td>".$row['mobile']."</td>
-<td>".$row['email']."</td>
-<td>".$row['qualification']."</td>
-
-<td><a href='uploads/".$row['photo']."' target='_blank'>View</a></td>
-
-<td><a href='uploads/".$row['signature']."' target='_blank'>View</a></td>
-
-<td><a href='uploads/".$row['marksheet']."' target='_blank'>Download</a></td>
-
-<td><a href='uploads/".$row['caste_certificate']."' target='_blank'>Download</a></td>
-
-</tr>";
-
+echo $row['name']."\t".
+$row['father_name']."\t".
+$row['mobile']."\t".
+$row['email']."\t".
+$row['qualification']."\t".
+$row['district']."\t".
+$row['state']."\t".
+$row['pincode']."\n";
 }
 
 ?>
-
-</table>
